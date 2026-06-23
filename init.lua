c25d2cde479b74af2126a876c98d955c29ef2898
the man of the hour <him@localhost>
2026-06-22 18:30:34 -0600

    relnum

--- init.lua
+++ init.lua
@@ -1,11 +1,11 @@
 vim.opt.showmode = false
-vim.opt.shell = "/bin/fish"
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.winborder = "rounded"
 vim.opt.signcolumn = "yes"
 vim.opt.number = true
+vim.opt.relativenumber = true
 vim.opt.mouse = ""
 vim.opt.cursorline = true
 vim.g.editorconfig = false
